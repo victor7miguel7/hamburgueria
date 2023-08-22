989afb6f2255e98fc3726e7178b4f685614b57ab
Victor Miguel <victor78miguel@gmail.com>
2023-08-21 21:38:21 -0300

    Ajustes para o Backend em python

--- backend/module1/app.js
+++ backend/module1/app.js
@@ -20,15 +20,39 @@ app.post('/api/orders', (req, res) => {
     res.json({ orderId: orderId, message: 'Pedido recebido com sucesso!' });
 });
 
-function generateOrderId() {
-    return new Date().getTime().toString();
-}
 
-function saveOrder(orderId, orderData) {
-    const fs = require('fs');
-    fs.writeFileSync(`orders/${orderId}.json`, JSON.stringify(orderData, null, 2));
-}
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+/*
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let body = document.querySelector('body');
+
+openShopping.addEventListener('click', () => {
+    body.classList.add('active');
+});
+
+closeShopping.addEventListener('click', () => {
+    body.classList.remove('active');
+});
+
+// Produtos (já estão no seu código)
+// Funções (já estão no seu código)
+
+function initApp() {
+    products.forEach((value, key) => {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Adiciona ao Carrinho</button>`;
+        list.appendChild(newDiv);
+    });
+}
+
+initApp();*/
+
